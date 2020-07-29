158baa2d2d735111cdf2dfdeafd868380cf46038
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-29 09:17:07 -0600

    Add test for attributes and pass

--- test/item_test.rb
+++ test/item_test.rb
@@ -10,6 +10,14 @@ class ItemTest < MiniTest::Test
     assert_instance_of Item, item1
   end
 
-  # item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  def test_it_has_attributes
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+    assert_equal "Tomato", item2.name
+    assert_equal 0.50, item2.price
+    assert_equal "Peach", item1.name
+    assert_equal 0.75, item1.price
+  end
 
 end

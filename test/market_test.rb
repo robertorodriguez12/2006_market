5f791def859b35c1a3f298b2f1f73350f639d091
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-29 09:48:32 -0600

    Add attributes test

--- test/market_test.rb
+++ test/market_test.rb
@@ -12,4 +12,10 @@ class MarketTest < MiniTest::Test
     assert_instance_of Market, market
   end
 
+  def test_it_has_attributes
+    market = Market.new("South Pearl Street Farmers Market")
+    assert_equal "South Pearl Street Farmers Market", market.name
+    assert_equal [], market.vendors
+  end
+
 end

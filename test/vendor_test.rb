dcb77d8453c777daf86d01ce907bc55e3f20cd4b
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-29 09:20:32 -0600

    Add attributes test

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -10,12 +10,21 @@ class VendorTest < MiniTest::Test
     assert_instance_of Vendor, vendor
   end
 
-  def test_it_can_add_items
+  def test_it_has_attributes
     vendor = Vendor.new("Rocky Mountain Fresh")
 
-
-    vendor.a
-
+    assert_equal "Rocky Mountain Fresh", vendor.name
+    expected = {}
+    assert_equal expected, vendor.inventory
   end
+
+  # def test_it_can_add_items
+  #   vendor = Vendor.new("Rocky Mountain Fresh")
+  #   item1 = Item.new({name: 'Peach', price: "$0.75"})
+  #   item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  #
+  #   vendor.
+  #
+  # end
 
 end

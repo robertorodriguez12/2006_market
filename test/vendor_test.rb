744b66aa9200b0ec3182d9b3a852bdb003431844
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-29 09:44:01 -0600

    Add test to stock item and check inventory

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -20,13 +20,20 @@ class VendorTest < MiniTest::Test
     assert_equal expected, vendor.inventory
     assert_equal 0, vendor.check_stock(item1)
   end
-  
-  def test_it_can_add_items
+
+  def test_it_can_stock_items
     vendor = Vendor.new("Rocky Mountain Fresh")
     item1 = Item.new({name: 'Peach', price: "$0.75"})
     item2 = Item.new({name: 'Tomato', price: '$0.50'})
 
-    vendor.
+    vendor.stock(item1, 30)
+    assert_equal 30, vendor.check_stock(item1)
+    vendor.stock(item1, 25)
+    assert_equal 55, vendor.check_stock(item1)
+    vendor.stock(item2, 12)
+    expected = {item1 => 55, item2 => 12}
+
+    assert_equal expected, vendor.inventory
 
   end
 

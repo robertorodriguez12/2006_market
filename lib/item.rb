6978889b2cbfda23561fa343906de0d7372870cf
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-29 09:17:27 -0600

    Add method to change price string to a float

--- lib/item.rb
+++ lib/item.rb
@@ -1,10 +1,14 @@
 class Item
 
   attr_reader :name, :price
-  
+
   def initialize(information)
     @name = information[:name]
     @price = information[:price]
+  end
+
+  def price
+    @price.split("$")[1].to_f
   end
 
 end

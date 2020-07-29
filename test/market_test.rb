c20ebdc335bcf76cfd0d1dfe433ec5aee14dc73d
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-29 10:39:07 -0600

    Add test to check potential revenue for vendors

--- test/market_test.rb
+++ test/market_test.rb
@@ -84,6 +84,76 @@ class MarketTest < MiniTest::Test
 
     expected = [vendor1, vendor3]
     assert_equal expected, market.vendors_that_sell(item1)
-    assert_equal [vendor]
+    assert_equal [vendor2], market.vendors_that_sell(item4)
+  end
+
+  def test_it_can_get_vendors_potential_revenue
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    vendor3.stock(item1, 65)
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+
+    assert_equal 29.75, vendor1.potential_revenue
+    assert_equal 345.00, vendor2.potential_revenue
+    assert_equal 48.75, vendor3.potential_revenue
+  end
+
+  def test_it_can_provide_total_inventory
+    pry(main)> market = Market.new("South Pearl Street Farmers Market")
+    #=> #<Market:0x00007fe134933e20...>
+
+    pry(main)> item1 = Item.new({name: "Peach", price: "$0.75"})
+    #=> #<Item:0x007f9c56740d48...>
+
+    pry(main)> item2 = Item.new({name: "Tomato", price: "$0.50"})
+    #=> #<Item:0x007f9c565c0ce8...>
+
+    pry(main)> item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    #=> #<Item:0x007f9c562a5f18...>
+
+    pry(main)> item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    #=> #<Item:0x007f9c56343038...>
+
+    pry(main)> vendor1 = Vendor.new("Rocky Mountain Fresh")
+    #=> #<Vendor:0x00007fe1348a1160...>
+
+    pry(main)> vendor1.stock(item1, 35)
+
+    pry(main)> vendor1.stock(item2, 7)
+
+    pry(main)> vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    #=> #<Vendor:0x00007fe1349bed40...>
+
+    pry(main)> vendor2.stock(item4, 50)
+
+    pry(main)> vendor2.stock(item3, 25)
+
+    pry(main)> vendor3 = Vendor.new("Palisade Peach Shack")
+    #=> #<Vendor:0x00007fe134910650...>
+
+    pry(main)> vendor3.stock(item1, 65)
+
+    pry(main)> vendor3.stock(item3, 10)
+
+    pry(main)> market.add_vendor(vendor1)
+
+    pry(main)> market.add_vendor(vendor2)
+
+    pry(main)> market.add_vendor(vendor3)
+
+    pry(main)> market.total_inventory
+
   end
 end

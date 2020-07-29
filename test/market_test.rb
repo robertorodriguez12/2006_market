63a6b5f253e0b33a42a47c2c1b81cc83d946521f
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-29 10:18:09 -0600

    Add test to check vendors that sell method

--- test/market_test.rb
+++ test/market_test.rb
@@ -37,7 +37,53 @@ class MarketTest < MiniTest::Test
     market.add_vendor(vendor3)
 
     assert_equal [vendor1, vendor2, vendor3], market.vendors
+  end
 
+  def test_it_can_name_the_vendors
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    vendor3.stock(item1, 65)
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
 
+    expected = ["Rocky Mountain Fresh",
+                "Ba-Nom-a-Nom",
+                "Palisade Peach Shack"]
+
+    assert_equal expected, market.vendor_names
+  end
+
+  def test_it_can_list_vendors_that_sell_a_certain_item
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    vendor3.stock(item1, 65)
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+
+    expected = [vendor1, vendor3]
+    assert_equal expected, market.vendors_that_sell(item1)
+    assert_equal [vendor]
   end
 end

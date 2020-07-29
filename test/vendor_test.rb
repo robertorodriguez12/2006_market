0b4dad437446bf2f6fa6c52f95721c577c539da7
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-29 09:34:17 -0600

    Add test to check stock on an empty item

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -12,19 +12,22 @@ class VendorTest < MiniTest::Test
 
   def test_it_has_attributes
     vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
 
     assert_equal "Rocky Mountain Fresh", vendor.name
     expected = {}
     assert_equal expected, vendor.inventory
+    assert_equal 0, vendor.check_stock(item1)
   end
+  
+  def test_it_can_add_items
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
 
-  # def test_it_can_add_items
-  #   vendor = Vendor.new("Rocky Mountain Fresh")
-  #   item1 = Item.new({name: 'Peach', price: "$0.75"})
-  #   item2 = Item.new({name: 'Tomato', price: '$0.50'})
-  #
-  #   vendor.
-  #
-  # end
+    vendor.
+
+  end
 
 end

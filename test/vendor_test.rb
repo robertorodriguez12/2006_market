31556bd94e44f6ab334c982cfb3272595e92250a
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-29 10:44:07 -0600

    Add test to check potential revenue

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -13,7 +13,6 @@ class VendorTest < MiniTest::Test
   def test_it_has_attributes
     vendor = Vendor.new("Rocky Mountain Fresh")
     item1 = Item.new({name: 'Peach', price: "$0.75"})
-    item2 = Item.new({name: 'Tomato', price: '$0.50'})
 
     assert_equal "Rocky Mountain Fresh", vendor.name
     expected = {}
@@ -37,4 +36,13 @@ class VendorTest < MiniTest::Test
 
   end
 
+  def test_vendor_can_get_potential_revenue
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    vendor.stock(item1, 30)
+    vendor.stock(item1, 25)
+    vendor.stock(item2, 12)
+    assert_equal 47.25, vendor.potential_revenue
+  end
 end
